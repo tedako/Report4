42b1f480cd9f3eaa31c7853966287528613e8678
Haruya Ura <e175738@ie.u-ryukyu.ac.jp>
2017-11-27 12:29:33 +0900

    プログラミング課題

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
 
 public class Main {
     public static void main(String[] args) {
-        String str = null;
-        System.out.println(str.length());
-        
+
+        String str;
+        try{
+            str = null;
+            System.out.println(str.length());
+        }catch (NullPointerException e){
+            System.out.println("例外が発生しました。");
+            System.out.println(e.getMessage());
+                  }
+
     }
 
 }
